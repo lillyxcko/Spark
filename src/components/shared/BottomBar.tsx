ecd7f663771dd423400367cfa6ffac4573eac305
Lilly Ko <lillyxcko@gmail.com>
2023-12-13 19:52:37 -0800

    bottombar complete

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { bottombarLinks } from '@/constants';
+import { Link, useLocation} from 'react-router-dom';
 
 const BottomBar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>BottomBar</div>
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+          const isActive = pathname === link.route;
+
+      return (
+        
+          <Link
+            to={link.route} key={link.label} className={` ${
+              isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img 
+              src ={link.imgURL}
+              alt ={link.label}
+              width={16}
+              height={16}
+              className = {`${isActive && 'invert-white'}`}
+            />
+              <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        
+            )
+      })}                        
+      
+    </section>
   )
 }
 
