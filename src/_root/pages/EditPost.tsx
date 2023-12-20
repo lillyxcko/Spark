6daa9ee314d218ed6baec2897adc38045336db71
Lilly Ko <lillyxcko@gmail.com>
2023-12-19 23:32:59 -0800

    starting post CRUD

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import PostForm from '@/components/forms/PostForm'
+import { useGetPostById } from '@/lib/react-query/queriesAndMutations';
+import { Loader } from 'lucide-react';
+import { useParams } from 'react-router-dom';
 
 const EditPost = () => {
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id || '');
+
+  if(isPending) return <Loader />
+
+
   return (
-    <div>EditPost</div>
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img 
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
+        </div>
+        <PostForm />
+
+      </div>
+    </div>
   )
 }
 
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getPostById, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
 import { CreatePost } from '@/_root/pages'
 import { QUERY_KEYS } from './queryKeys'
@@ -135,3 +135,11 @@ export const useGetCurrentUser = () => {
       queryFn: getCurrentUser,
     });
   };
+
+export const useGetPostById = (postId: string) => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+        queryFn: () => getPostById(postId),
+        enabled: !!postId
+    })
+}
