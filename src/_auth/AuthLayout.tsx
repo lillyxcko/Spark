c0eed272a8e3414d3a9fedef81b40933a71625ce
Lilly Ko <lillyxcko@gmail.com>
2024-01-06 22:52:26 -0800

    image change

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
         </section>
 
         <img 
-          src="/assets/images/side-img.svg"
+          src="/assets/images/side-image.jpg"
           alt="logo"
           className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
         />
